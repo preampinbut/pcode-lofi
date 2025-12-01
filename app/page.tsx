e46fda20416703ab24def24c0af3ea7ff1c62da1
Pream Pinbut <preampinbut@outlook.co.th>
2025-12-01 22:56:24 +0700

    force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import App from "./app";
 import prisma from "@/app/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const channels = await prisma.channel.findMany({
     orderBy: {
